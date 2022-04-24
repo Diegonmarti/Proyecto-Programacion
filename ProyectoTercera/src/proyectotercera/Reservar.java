2184a76b44e58ad7bd87ad3f24e1de19d0dcbd5e
KeyKoder <victor.fernandez.montano@gmail.com>
2022-04-24 19:17:01 +0200

    Eliminado archivos de prueba.

--- ProyectoTercera/src/proyectotercera/Reservar.java
+++ ProyectoTercera/src/proyectotercera/Reservar.java
@@ -1,61 +1,98 @@
 package proyectotercera;
 
-import proyectotercera.ReservarConst;
-
+import java.util.Scanner;
+ 
 public class Reservar {
 
-    private ReservarConst[] reservas;
+    //private static Reservar[] cita;
+    private static Scanner entrada;
 
-    public Reservar() {
-        reservas = new ReservarConst[3];
-    }
-    
-    //private int getPosArray(String hora) {
-    //    for(int i=0;i<reservas.length;i++){
-    //        if(hora.equals(i){
-    //            return i;
-    //        }else{
-    //            return -1; //En realidad tendría que saltar una exception o poner un mensaje de error
-    //        }
-    //    }
-   // }
-    
-    private int getPosArray(String hora) {
-        if (hora.equals("9:00")) {
-            return 0;
-        } else if (hora.equals("10:00")) {
-            return 1;
-        } else {
-            return 2;
-        }
-    }
+    public static void main(String[] args) {
+        entrada = new Scanner(System.in);
+        /*cita = new Reservar[10];
+        for (int i = 0; i < cita.length; i++) {
+            cita[i] = new Reservar();
+        }*/
 
-    public void hacerReserva(ReservarConst reserva) {
-        int pos = getPosArray(reserva.getApellido());
-        if (reservas[pos] != null) {
-            System.out.println("Esta HORA ya está reservada");
-        } else {
-            reservas[pos] = reserva;
-            System.out.println("RESERVA efectuada");
-        }
-    }
+        Scanner entrada = new Scanner(System.in);
+        String palabra;
+        do {
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            System.out.println(">Programa de reservas de Tutorías>");
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            System.out.println("Introduce el nombre del fichero de reservas a leer");
+            //Hacer el Scanner que llame al fichero
 
-    public void anularReserva(String nombre, String hora) {
-        int pos = getPosArray(hora);
-        if (reservas[pos] != null) {
-            if (reservas[pos].getNombre().equals(nombre)) {
-                reservas[pos] = null;
-                System.out.println("RESERVA anulada");
-            } else {
-                System.out.println("EL NOMBRE no se corresponde con la persona que hizo la RESERVA");
+            System.out.println("1. RESERVAR");
+            System.out.println("2. ANULAR");
+            System.out.println("3. CONSULTAR");
+            System.out.println("4. SALIR");
+            palabra = entrada.nextLine().toUpperCase();
+         
+            switch (palabra) {
+
+                case "RESERVAR":
+                    ReservarCita();
+                    break;
+                case "ANULAR":
+                    AnularCita();
+                    break;
+                case "CONSULTAR":
+                    ConsultarCita();
+                    // menuConsultar
+                    break;
+                case "SALIR":
+                    System.out.println("Salir del programa");
+                    break;
+                default:
+                    System.out.println("Opción errónea...\n");
             }
-        } else {
-            System.out.println("No hay ninguna RESERVA para la HORA indicada");
+        } while (!palabra.equals("SALIR"));
+    }
+
+    private static void ReservarCita() {
+        System.out.print("\nIndique Citas: ");
+        int citas = Integer.parseInt(entrada.nextLine());
+        System.out.print("Hora: ");
+        String hora = entrada.nextLine();
+        System.out.println("\n\tDatos de la reserva");
+        System.out.print("Nombre: ");
+        String nombre = entrada.nextLine();
+        System.out.print("Di tu mail: ");
+        String mail = entrada.nextLine();
+        System.out.print("Teléfono: ");
+        String telefono = entrada.nextLine();
+
+        // cita[citas].hacerReserva(new ReservarConst(nombre, hora, mail, telefono));
+
+    }
+
+    private static void AnularCita() {
+        System.out.print("\nIndique cita para anular: ");
+        int citas = Integer.parseInt(entrada.nextLine());
+        System.out.print("Hora que quiera anular: ");
+        String hora = entrada.nextLine();
+        System.out.print("Nombre del archivo para anular: ");
+        String nombre = entrada.nextLine();
+
+        // cita[citas].anularReserva(nombre, hora);
+    }
+
+    private static void ConsultarCita() {
+        System.out.print("Hora a reservar: ");
+        String hora = entrada.nextLine();
+        boolean hayDisponibles = false;
+
+        System.out.println("\nCitas disponibles: ");
+        /*for (int i = 0; i < cita.length; i++) {
+            if (cita[i].estaDisponible(hora)) {
+                System.out.println("- Citas #" + i);
+                hayDisponibles = true;
+            }
+        }*/
+
+        if (!hayDisponibles) {
+            System.out.println("\nNo hay citas disponibles para esa hora");
         }
     }
-
-    public boolean estaDisponible(String hora) {
-        return reservas[getPosArray(hora)] == null;
-    }
-
 }
