9232b0fd1e1e33618879fe40620d4dbf71db0f1e
juanjo003 <jjmlj10@gmail.com>
2022-05-04 19:08:38 +0200

    Comentado MetodosComunes

--- ProyectoTercera/src/proyectotercera/utils/MetodosComunes.java
+++ ProyectoTercera/src/proyectotercera/utils/MetodosComunes.java
@@ -8,13 +8,29 @@ public abstract class MetodosComunes {
         String nombre = "";
         boolean fin = false;
         boolean primeraIteracion = true;
+        
         while (!fin) {
+            // para que ponga otro mensaje si se vuelve a preguntar (si falla o si no sobreescribe)
             if(!primeraIteracion) {
                 System.out.println(mensaje);
             }
             primeraIteracion = false;
             nombre = entrada.nextLine().trim(); //meter el nombre del fichero
             if(nombre.length() > 0) {  //Al menos meter un carácter
+                /* explicacion regex
+                    .* -> cualquier caracter (0 + veces)
+                    [(...)] -> cualquiera de estos caracteres:
+                        \\ -> "\"
+                        /
+                        :
+                        \* -> "*"
+                        \? -> "?"
+                        \" -> el caracter "
+                        <
+                        >
+                        \| -> "|"
+                    .* -> cualquier caracter (0 + veces)
+                */
                 if(nombre.matches(".*[\\\\/:\\*\\?\"<>\\|].*")){ //que no meta ninguno de estos símbolos
                     System.out.println("ERROR: Nombre de archivo no válido.");
                 }else {  //si lo mete todo bien, puede salir
@@ -22,7 +38,7 @@ public abstract class MetodosComunes {
                         nombre += ".txt";
                     }
 
-                    if(FileUtils.existe(nombre)) {
+                    if(FileUtils.existe(nombre)) {//si colocas el nombre de un archivo que ya existe 
                         System.out.println("El fichero " + nombre + " ya existe, si desea sobreescribirlo, introduzca SI.");
                         String opcion = entrada.nextLine();
 
